600c0bb7aa71cdb7ec5e0445b9ca3a51b417631a
Dolly Aswin <dolly.aswin@gmail.com>
2016-09-03 16:22:13 +0700

    Nambah method void test()

--- app/src/main/java/com/example/ali/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ali/myapplication/MainActivity.java
@@ -12,4 +12,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    protected void test()
+    {
+    }
 }
