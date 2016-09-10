b0982d25cefe01674483f0a7d21b31d474840e7e
razoryo17 <ali.hudzaifah@gmail.com>
2016-09-10 17:23:26 +0700

    Editext,Textview and Button

--- app/src/main/java/com/example/ali/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ali/myapplication/MainActivity.java
@@ -2,22 +2,29 @@ package com.example.ali.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText editText;
+    TextView txtvi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.onCreate(savedInstanceState);
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText)findViewById(R.id.edt1);
+        txtvi = (TextView)findViewById(R.id.textid);
+
     }
 
-    protected void test()
-    {
-    }
-   protected void again()
-    {
+    public void btn(View view){
+
+        String temp;
+        temp = editText.getText().toString();
+        txtvi.setText(temp);
+
     }
 }
-
